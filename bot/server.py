c3ec9756feeadc12a1fe3fd928715b96bab9a333
VashchilkoAV <vashchilko.av@phystech.edu>
2021-03-13 01:09:35 +0300

    for test different ways of authorization

--- bot/server.py
+++ bot/server.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 client_id = "7562746"
 redirect_url = "http://localhost:8000/auth_complete"
 v = "5.130"
-basic_url = "https://oauth.vk.com/access_token"
+basic_url = "https://oauth.vk.com/access_token" #for access_token
+#basic_url = "https://oauth.vk.com/authorize" #for code
 grant_type = "client_credentials"
 client_secret = "rqozJXDcBPrygXS181xr"
 url = basic_url+"?client_id="+client_id+"&v="+v+"&redirect_uri="+redirect_url+"&grant_type="+grant_type+"&client_secret="+client_secret
