d51cf4c855b7753f744347c2d1d85061e808800e
VashchilkoAV <vashchilko.av@phystech.edu>
2021-03-18 23:49:21 +0300

    bot with authorization

--- bot/server.py
+++ bot/server.py
@@ -1,11 +1,11 @@
 from flask import Flask, session, request, redirect
 import vk_api, datetime
 from flask_session import Session
-from Music_analyzer.vk_music_analyzer import vk_music_analyzer
+#from Music_analyzer.vk_music_analyzer import vk_music_analyzer
 from vk_api.audio import VkAudio
 
 
-client_id = "7562746"
+client_id = "7794879"
 
 
 redirect_url_end = "/auth_complete"
@@ -14,7 +14,7 @@ v = "5.130"
 basic_url_for_token = "https://oauth.vk.com/access_token" #for access_token
 basic_url_for_code = "https://oauth.vk.com/authorize" #for code
 grant_type = "client_credentials"
-client_secret = "zS76Cc47jUR83n01CesN"
+client_secret = "SsVRgiVPrle4mhxR3aOd"
 responce_type = "code"
 scope = "audio,email"
 
@@ -34,9 +34,7 @@ def auth():
 @app.route(redirect_url_end)
 def auth_complete():
     #tg_id = request.args.get('state')
-    code = request.args.get('code')
-    print(code) 
-    """
+    code = request.args.get('code') 
     vk_session = vk_api.VkApi(app_id=client_id, client_secret=client_secret, scope = '8')
 
     try:
@@ -46,19 +44,13 @@ def auth_complete():
         return
     
 
-    email = vk_session.token['email']
-    access_token = vk_session.token['access_token']
-    vk_session = vk_api.VkApi(login = email, token = access_token)
+    vk = vk_session.get_api()
+    resp = vk.users.get(v=5.89, name_case="Nom")
+    print(resp)
 
-    try:
-        vk_session.auth(token_only = True)
-    except vk_api.AuthError as error_msg:
-        print(error_msg)
-        return
-
-    vk = vk_music_analyzer()
-    print(email)
-    print(vk.get_favourite_artists(vk_session))
+    #vk = vk_music_analyzer()
+    #print(email)
+    #print(vk.get_favourite_artists(vk_session))
     #add this pair to db
     #if code:
     #    return redirect(url2+code)
@@ -66,7 +58,6 @@ def auth_complete():
     #    email = request.args.get('email')
     #    access_token = request.args.get('access_token')
     #    print(email, access_token)
-    """
     return "good"
 
 
