ff7523955464238d90e92ec6497247b1605b1740
VashchilkoAV <vashchilko.av@phystech.edu>
2021-04-13 12:07:58 +0300

    logger

--- bot/music_geek_bot.py
+++ bot/music_geek_bot.py
@@ -9,6 +9,22 @@ from Concerts.yandex_afisha_concerts import Concerts
 import json
 
 
+class Unbuffered(object):
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+   def writelines(self, datas):
+       self.stream.writelines(datas)
+       self.stream.flush()
+   def __getattr__(self, attr):
+       return getattr(self.stream, attr)
+
+import sys
+sys.stdout = Unbuffered(sys.stdout)
+
+
 CONFIG_FILE = './bot/config.json'
 
 with open(CONFIG_FILE) as conf:
--- logger/logger.py
+++ logger/logger.py
@@ -4,14 +4,30 @@ TOKEN = "1713650415:AAHpmqcTLVPFaoOtTi_POHk6Z66j3tr3ac4"
 bot = telebot.TeleBot(TOKEN)
 CHAT_ID = -560631730
 
-FILENAME = "log.fifo"
 
-fd = open(FILENAME,"r")
+LOGS_PATH = "/home/testServer/musicGEEK/logs"
+DBO_FNAME = "db.o"
+DBE_FNAME = "db.e"
+BOTO_FNAME = "bot.o"
+BOTE_FNAME = "bot.e"
+SERVERO_FNAME = "server.o"
+SERVERE_FNAME = "server.e"
+
+
+
+
+#dbo_fd = open(LOGS_PATH+DBO_FNAME,"r")
+#dbe_fd = open(LOGS_PATH+DBE_FNAME,"r")
+boto_fd = open(LOGS_PATH+BOTO_FNAME,"r")
+#bote_fd = open(LOGS_PATH+BOTE_FNAME,"r")
+#servero_fd = open(LOGS_PATH+SERVERO_FNAME,"r")
+#servere_fd = open(LOGS_PATH+SERVERE_FNAME,"r")
+
 
 
 
 
 while True:
-    line = fd.readline()
-    if line != "":
-        bot.send_message(CHAT_ID, line)
+    boto_line = boto_fd.readline()
+    if boto_line != "":
+        bot.send_message(CHAT_ID, boto_line)
