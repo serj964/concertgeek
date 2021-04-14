cf7f1b958b3a8411d0af48111940082fe9dfbf00
LightVillet <maxim12000@ya.ru>
2021-04-14 15:03:26 +0300

    Concerts add cities | improve find_concerts

--- Concerts/yandex_afisha_concerts.py
+++ Concerts/yandex_afisha_concerts.py
@@ -1,13 +1,47 @@
 import requests
 import datetime
+import re
 
 
 class Concerts:
     def __init__(self):
         self.concerts = []
+        self.cities = {'Москва': 'moscow',
+                       'Санкт-Петербург': 'saint-petersburg',
+                       'Сочи': 'sochi'
+                       'Краснодар': 'krasnodar',
+                       'Казань': 'kazan',
+                       'Новосибирск': 'novosibirsk',
+                       'Екатеринбург': 'yekaterinburg',
+                       'Нижний Новгород': 'nizhny-novgorod',
+                       'Челябинск': 'chelyabinsk',
+                       'Владивосток': 'vladivostok',
+                       'Белгород': 'belgorod',
+                       'Владимир': 'vladimir',
+                       'Ижевск': 'izhevsk',
+                       'Красноярск': 'krasnoyarsk',
+                       'Мурманск': 'murmansk',
+                       'Смоленск': 'smolensk',
+                       'Омск': 'omsk',
+                       'Хабаровск': 'khabarovsk',
+                       'Самара': 'samara',
+                       'Ростов-на-Дону': 'rostov-na-donu',
+                       'Уфа': 'ufa',
+                       'Воронеж': 'voronezh',
+                       'Пермь': 'perm',
+                       'Волгоград': 'volgograd',
+                       'Тула': 'tula',
+                       'Рязань': 'ryazan',
+                       'Калуга': 'kaluga',
+                       'Тверь': 'tver'
+        }
 
-    def load_concerts(self, city='moscow', day=datetime.date.today(), number_of_days=30):
+    def load_concerts(self, city='Москва', day=datetime.date.today(), number_of_days=30):
         self.concerts = []
+        try:
+            city = self.cities[city]
+        except Exception:
+            city = 'moscow'
         limit = 20
         offset = 0
         link = f"https://afisha.yandex.ru/api/events/rubric/concert?limit={limit}&offset={offset}&hasMixed=0&date={day.strftime('%Y-%m-%d')}&period={number_of_days}&city={city}&_=1615390892410"
@@ -46,6 +80,6 @@ class Concerts:
     def find_concerts(self, artist):
         suitable_concerts = []
         for concert in self.concerts:
-            if concert['title'].lower() == artist.lower():
-                suitable_concerts.append(concert)
+            if re.search(artist.lower(), concert['title'].lower()):
+             suitable_concerts.append(concert)
         return suitable_concerts
