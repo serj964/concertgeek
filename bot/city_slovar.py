8d492dabfa897dcb6d4f5e5177dfc98e68c65491
LightVillet <maxim12000@ya.ru>
2021-05-05 11:28:21 +0300

    refactoring city_slovar.py

--- bot/city_slovar.py
+++ bot/city_slovar.py
@@ -1,47 +1,44 @@
-import math
+from math import cos, radians, acos, sin
 
 class City_slovar:
     def __init__(self):
-        self.SLOVAR = {'Москва': 'moscow; 55.755814, 37.617635',
-                       'Санкт-Петербург': 'saint-petersburg; 59.939095, 30.315868',
-                       'Сочи': 'sochi; 43.585472, 39.723089',
-                       'Краснодар': 'krasnodar; 45.035470, 38.975313',
-                       'Казань': 'kazan; 55.796127, 49.106405',
-                       'Новосибирск': 'novosibirsk; 55.030199, 82.920430',
-                       'Екатеринбург': 'yekaterinburg; 56.838011, 60.597465',
-                       'Нижний Новгород': 'nizhny-novgorod; 56.331927, 44.023225',
-                       'Челябинск': 'chelyabinsk; 55.159897, 61.402554',
-                       'Владивосток': 'vladivostok; 43.115536, 131.885485',
-                       'Белгород': 'belgorod; 50.595414, 36.587268',
-                       'Владимир': 'vladimir; 56.129057, 40.406635',
-                       'Ижевск': 'izhevsk; 56.852676, 53.206891',
-                       'Красноярск': 'krasnoyarsk; 56.010563, 92.852572',
-                       'Мурманск': 'murmansk; 68.970663, 33.074909',
-                       'Смоленск': 'smolensk; 54.782630, 32.045287',
-                       'Омск': 'omsk; 54.989342, 73.368212',
-                       'Хабаровск': 'khabarovsk; 48.480223, 135.071917',
-                       'Самара': 'samara; 53.195873, 50.100193',
-                       'Ростов-на-Дону': 'rostov-na-donu; 47.222078, 39.720349',
-                       'Уфа': 'ufa; 54.735147, 55.958727',
-                       'Воронеж': 'voronezh; 51.660781, 39.200269',
-                       'Пермь': 'perm; 58.010450, 56.229434',
-                       'Волгоград': 'volgograd; 48.707067, 44.516975',
-                       'Тула': 'tula; 54.193122, 37.617348',
-                       'Рязань': 'ryazan; 54.629560, 39.741908',
-                       'Калуга': 'kaluga; 54.513845, 36.261215',
-                       'Тверь': 'tver; 56.859625, 35.911851',
-                       'Брянск': 'bryansk; 53.243556, 34.363425',
-                       'Курск': 'kursk; 51.730846, 36.193015'
+        self.SLOVAR = {'Москва': ['moscow', 55.755814, 37.617635],
+                       'Санкт-Петербург': ['saint-petersburg', 59.939095, 30.315868],
+                       'Сочи': ['sochi', 43.585472, 39.723089],
+                       'Краснодар': ['krasnodar', 45.035470, 38.975313],
+                       'Казань': ['kazan', 55.796127, 49.106405],
+                       'Новосибирск': ['novosibirsk', 55.030199, 82.920430],
+                       'Екатеринбург': ['yekaterinburg', 56.838011, 60.597465],
+                       'Нижний Новгород': ['nizhny-novgorod', 56.331927, 44.023225],
+                       'Челябинск': ['chelyabinsk', 55.159897, 61.402554],
+                       'Владивосток': ['vladivostok', 43.115536, 131.885485],
+                       'Белгород': ['belgorod', 50.595414, 36.587268],
+                       'Владимир': ['vladimir', 56.129057, 40.406635],
+                       'Ижевск': ['izhevsk', 56.852676, 53.206891],
+                       'Красноярск': ['krasnoyarsk', 56.010563, 92.852572],
+                       'Мурманск': ['murmansk', 68.970663, 33.074909],
+                       'Смоленск': ['smolensk', 54.782630, 32.045287],
+                       'Омск': ['omsk', 54.989342, 73.368212],
+                       'Хабаровск': ['khabarovsk', 48.480223, 135.071917],
+                       'Самара': ['samara', 53.195873, 50.100193],
+                       'Ростов-на-Дону': ['rostov-na-donu', 47.222078, 39.720349],
+                       'Уфа': ['ufa', 54.735147, 55.958727],
+                       'Воронеж': ['voronezh', 51.660781, 39.200269],
+                       'Пермь': ['perm', 58.010450, 56.229434],
+                       'Волгоград': ['volgograd', 48.707067, 44.516975],
+                       'Тула': ['tula', 54.193122, 37.617348],
+                       'Рязань': ['ryazan', 54.629560, 39.741908],
+                       'Калуга': ['kaluga', 54.513845, 36.261215],
+                       'Тверь': ['tver', 56.859625, 35.911851],
+                       'Брянск': ['bryansk', 53.243556, 34.363425],
+                       'Курск': ['kursk', 51.730846, 36.193015]
                 }
 
-    def __rad(self, x):
-        return math.radians(x)
-    
     
     def __measure(self, user_lat, user_long, city_lat, city_long):
-        a = math.cos(self.__rad(city_lat))*math.cos(self.__rad(user_lat))*math.cos(self.__rad(user_long)-self.__rad(city_long))
-        b = math.sin(self.__rad(city_lat))*math.sin(self.__rad(user_lat))
-        return 6367*math.acos(a + b)
+        a = cos(radians(city_lat))*cos(radians(user_lat))*cos(radians(user_long)-radians(city_long))
+        b = sin(radians(city_lat))*sin(radians(user_lat))
+        return 6367*acos(a + b)
     
     
     #возвращает по координатам
@@ -50,8 +47,8 @@ class City_slovar:
         city_dicti = {}
         for city in self.SLOVAR.keys():
             s = self.SLOVAR[city]
-            lat = float(s[s.find(';', 0,len(s))+2:s.find(',', 0,len(s))])
-            long = float(s[s.find(',', 0,len(s))+2:len(s)])
+            lat = float(s[1])
+            long = float(s[2])
             dist = self.__measure(user_lat, user_long, lat, long)
             dicti[city] = dist
             
@@ -63,7 +60,7 @@ class City_slovar:
                 dist = dicti[city]
         
         s = self.SLOVAR[nearest]
-        city_dicti[nearest] = s[0:s.find(';', 0,len(s))]
+        city_dicti[nearest] = s[0]
                 
         return city_dicti
     
@@ -74,9 +71,7 @@ class City_slovar:
         for city in self.SLOVAR.keys():
             if name.lower() == city.lower():
                 s = self.SLOVAR[city]
-                city_name = s[0:s.find(';', 0,len(s))]
-            else:
-                pass
+                city_name = s[0]
             
         if city_name != '':
             return city_name
